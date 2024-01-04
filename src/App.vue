9c35a6f9a0da89808b631e8685d8a92f06028aed
lyon-158 <2779146582@qq.com>
2024-01-04 13:54:12 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- html code  -->
+    <div class="app">
+        <h1>你好，VUE3</h1>
+    </div>
+</template>
+
+<script lang="ts">
+// js code
+    export default{
+        name:'App'
+    }
+</script>
+
+<style>
+/* css code */
+.app{
+    background-color: #ffe4c4bd;
+    box-shadow: 0 10px 20px;
+    border-radius: 9px;
+    padding: 10px;
+}
+</style>
